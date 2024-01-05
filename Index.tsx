ce1d7fe58ff661784ba7d90922994090211aa2d3
tahasama <taha.maatof@gmail.com>
2024-01-05 14:38:13 +0100

    try styling notifications

--- Index.tsx
+++ Index.tsx
@@ -27,7 +27,7 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
   }),
 });
 
