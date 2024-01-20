b8dab1ac0c4347576e6f278a0a3c24abe7939a82
tahasama <taha.maatof@gmail.com>
2024-01-20 23:05:34 +0100

    dark mode upgrade

--- Profile/LightDarkSwitch.tsx
+++ Profile/LightDarkSwitch.tsx
@@ -7,17 +7,23 @@ const LightDarkSwitch = ({ setDarkMode, isDarkMode }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => setDarkMode(false)}
+        onPress={() => setDarkMode(!isDarkMode)}
         style={styles.button}
       >
-        <LinearGradient colors={["#ffffff", "#f0f0f0"]} style={styles.gradient}>
+        <LinearGradient
+          colors={["#ffffff", "#f0f0f0"]}
+          style={[styles.gradient, styles.leftButton]}
+        >
           <Entypo name="light-up" size={24} color="black" />
           <Text>Light Mode</Text>
         </LinearGradient>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => setDarkMode(true)} style={styles.button}>
-        <LinearGradient colors={["#4d4d4d", "#000000"]} style={styles.gradient}>
+        <LinearGradient
+          colors={["#4d4d4d", "#000000"]}
+          style={[styles.gradient, styles.rightButton]}
+        >
           <Feather name="moon" size={24} color="white" />
           <Text style={{ color: "white" }}>Dark Mode</Text>
         </LinearGradient>
@@ -29,20 +35,30 @@ const LightDarkSwitch = ({ setDarkMode, isDarkMode }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "center",
     marginVertical: 10,
+    width: "100%",
   },
   button: {
     flexDirection: "row",
     alignItems: "center",
-    borderRadius: 10,
-    padding: 10,
+    flex: 1,
   },
   gradient: {
     flexDirection: "row",
     alignItems: "center",
-    borderRadius: 10,
+    justifyContent: "space-evenly",
     padding: 10,
+    flex: 1,
+    borderBottomStartRadius: 20,
+  },
+  leftButton: {
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  rightButton: {
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
   },
 });
 
